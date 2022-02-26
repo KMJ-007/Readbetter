52a7b58a600bffeb078f93838dffb9e9d676bd8f
Karann Janthe <karanjanthe@gmail.com>
2022-02-26 14:24:06 +0530

    commented cors

--- RB-be/server.js
+++ RB-be/server.js
@@ -35,13 +35,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 require(path.join(__dirname, './config/passport-setup'))(passport); //Load passport config
 // set up cors to allow us to accept requests from our client
-app.use(
-  cors({
-    origin: "http://localhost:3000", // allow to server to accept request from different origin
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true // allow session cookie from browser to pass through
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000", // allow to server to accept request from different origin
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     credentials: true // allow session cookie from browser to pass through
+//   })
+// );
 
 // set up routes
 app.use("/auth", authRoutes);
