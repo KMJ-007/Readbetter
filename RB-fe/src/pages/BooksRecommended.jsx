cce02f1a62e6cc657dde0c8c7f0767acdf440abe
krishna404 <agarwalbalkrishnaindia@gmail.com>
2022-02-27 12:41:12 +0530

    Recent frontend changes

--- RB-fe/src/pages/BooksRecommended.jsx
+++ RB-fe/src/pages/BooksRecommended.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react'
-import {Box, Button, Heading, Flex, Image, Center} from '@chakra-ui/react'
+import {Box, Button, Heading, Flex, Image, Center, HStack, Stack, Text} from '@chakra-ui/react'
 import {FaTwitter} from 'react-icons/fa'
 import logo from '../android-chrome-192x192.png'
 import {useParams} from 'react-router-dom'
-// import {localBE} from '../../offline-be'
+import {localBE} from '../../offline-be'
 
 
 function BooksRecommended(params) {
@@ -13,27 +13,29 @@ function BooksRecommended(params) {
 
   console.log({userHandle, resData})
 
+  const fullCardSize = 20;
+  const tileSize = "4em";
 
-  // useEffect(() => {
-  //   if(!resData){
-  //     fetch(('https://readbetter-backend.vercel.app/user/')+userHandle.twitterHandle,{ mode: 'no-cors', })
-  //     .then(res=>res.json())
-  //     .then((res)=>{
-  //       console.log(res);
-  //       setResData(res);
-  //     })
+
+  useEffect(async () => {
+    if(!resData){
+      // fetch(('https://readbetter-backend.vercel.app/user/')+userHandle.twitterHandle,{ mode: 'no-cors', })
+      // .then(res=>res.json())
+      // .then((res)=>{
+      //   console.log(res);
+      //   setResData(res);
+      // })
+
+      const books = await fetch(
+        (localBE || "https://readbetter-backend-kmj-007.vercel.app/")+"user/"+userHandle.twitterHandle);
+      let response = await books.json();
+      console.log(response);
+      setResData(response);
       
-  //   }
-  // },[resData]);
+    }
+  },[resData]);
     
-  async function getBookdata() {
-    const books = await fetch(
-        ("https://readbetter-backend-kmj-007.vercel.app/user")+userHandle.twitterHandle);
-    let response = await books.json();
-    console.log(response);
-}
-getBookdata();
-  if (!resData || !resData.status) {
+  if (!resData) {
     return (
       <Center h="100vh">
         <Box>
@@ -46,16 +48,84 @@ getBookdata();
       </Center>
     )
   }
-  return ( JSON.stringify(resData)
-    // <Center h="100vh">
-    //   <Box>
-    //     <Center flexDirection="column">
-          
-    //       <Heading align="center">Add Recommendation Here</Heading>
-          
-    //     </Center>
-    //   </Box>  
-    // </Center>
+  return ( 
+    <Center h="100vh" flexDirection="column" >
+      <Box width={fullCardSize+"em"} height={fullCardSize+"em"} background="lightblue">
+        <Center id="firstRow" flexDirection="row">
+          <Box width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"}>
+            <Flex flexDirection="row">
+              <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" borderRadius='full' boxSize={(fullCardSize/5/3)+"em"} />
+              <Text height={(fullCardSize/5/3)+"em"}>@saurabh</Text>
+            </Flex>
+            <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+            <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+          </Box>
+          <Box width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"}/>
+          <Box width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"}>
+            <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+            <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+            <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+          </Box>
+          <Box width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"}/>
+          <Box width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"}>
+            <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+            <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+            <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+          </Box>
+        </Center>
+        <HStack id="secondRow">
+          <Box width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"}/>
+          <Image src={resData[0].imageUrl} borderRadius='full' boxSize={(fullCardSize/5)+"em"} />
+          <Image src={resData[1].imageUrl} width={tileSize} height={tileSize}/>
+          <Image src={resData[2].imageUrl} width={tileSize} height={tileSize}/>            
+          <Box width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"}/>
+        </HStack>
+        <HStack id="thirdRow" flexDirection="row">
+            <Box width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"}>
+              <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+              <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+              <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+            </Box>
+            <Image src={resData[0].imageUrl} width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"} />
+            <Image src="https://pbs.twimg.com/profile_images/1315183823890522113/xxA4PPIn_400x400.jpg" width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"}/>
+            <Image src={resData[0].imageUrl} width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"} />
+            <Box width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"}>
+              <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+              <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+              <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+            </Box>
+          </HStack>
+          <HStack id="fourthRow" flexDirection="row">
+          <Box width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"}/>
+          <Image src={resData[0].imageUrl} width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"} />
+          <Image src={resData[0].imageUrl} width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"} />
+          <Image src={resData[0].imageUrl} width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"} />
+          <Box width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"}/>
+        </HStack>
+        <HStack id="fifthRow" flexDirection="row">
+          <Box width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"}>
+            <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+            <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+            <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+          </Box>
+          <Box width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"}/>
+          <Box width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"}>
+            <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+            <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+            <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+          </Box>
+          <Box width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"}/>
+          <Box width={(fullCardSize/5)+"em"} height={(fullCardSize/5)+"em"}>
+            <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+            <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+            <Image src="https://pbs.twimg.com/profile_images/534887249658142720/f6ZudLDi_400x400.jpeg" width={(fullCardSize/5/3)+"em"} height={(fullCardSize/5/3)+"em"}></Image>
+          </Box>
+        </HStack>
+      </Box>
+      {/* <Box>
+        {JSON.stringify(resData)}
+      </Box> */}
+    </Center>
   )
 
 }
